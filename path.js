2af89a5de67ce3158cc3d8a5f417293fa2bb9a3a
Shahid Ahamed <shahid.07ahamed@gmail.com>
2025-11-29 18:26:43 +0900

    path.js

---
+++ path.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+console.log("file updateing...\n");
+
+console.log("filename...",__filename);
+console.log("Directory:",__dirname);
+
+console.log("\n"+"-".repeat(57)+"\n");
+
+const filePath = "/shahid/documents/data.pdf";
+
+console.log(filePath);
