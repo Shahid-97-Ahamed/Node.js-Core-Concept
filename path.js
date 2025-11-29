ef25d204f31dbe590b0923b532673c2d7cc16368
Shahid Ahamed <shahid.07ahamed@gmail.com>
2025-11-29 22:24:29 +0900

    file-organizer.js

--- path.js
+++ path.js
@@ -9,4 +9,27 @@ console.log("\n"+"-".repeat(57)+"\n");
 
 const filePath = "/shahid/documents/data.pdf";
 
-console.log(filePath);
+console.log("Analyzing path",filePath,"\n");
+
+/*
+1.Directory name
+2.Base Name
+3.file Extention
+4.file name
+*/
+
+console.log("Directory name",path.dirname(filePath));
+console.log("Base-Name:",path.basename(filePath));
+console.log("file-extantion",path.extname(filePath));
+console.log("file-name:",path.basename(filePath,path.extname(filePath)));
+
+console.log("\n"+"-".repeat(57)+"\n");
+
+const persed =path.parse(filePath);
+console.log(persed);
+
+console.log("\n"+"-".repeat(57)+"\n");
+
+// for formatting....
+
+console.log("formating",path.format(persed));
